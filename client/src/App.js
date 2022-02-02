1d80eef605a0c7f62216bb5766d598f37252b28d
Hoa Le <Hoa_le9485@yahoo.com>
2022-02-02 18:27:23 -0500

    blog title deployment test

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 
+import { useState } from 'react';
+import { useEffect } from 'react/cjs/react.development';
 import './App.css';
 
 function App() {
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    fetch("/blogs")
+    .then(r => r.json())
+    .then(data => setBlogs(data))
+  }, [])
+
+  console.log(blogs)
+
   return (
     <div className="App">
-      Lets learn react test deployment
+      Lets learn react test deployment  
+      {blogs.map((blog) => {
+        return blog.title
+      })}
     </div>
   );
 }
